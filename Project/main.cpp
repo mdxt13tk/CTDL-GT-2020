a3649c2a74eb924e554f84e71330ede304758a38
kenvin1508 <votungnghia1508@gmail.com>
2020-05-10 14:54:45 +0700

    Push force project

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,28 +1,13 @@
-#include<graphics.h>
-#include<conio.h>
-#include<dos.h>
+/*	Test thu vien winbgim.h	*/
 
-main()
+#include <winbgim.h> 
+
+main(int argc, char *argv[])
 {
-   int gd = DETECT, gm, x, y, color, angle = 0;
-   struct arccoordstype a, b;
-   initgraph(&gd, &gm, "C:\\TC\\BGI");
-   delay(2000);                                                                
-   while(angle<=360)
-   {
-      setcolor(BLACK);
-      arc(getmaxx()/2,getmaxy()/2,angle,angle+2,100);
-      setcolor(RED);
-      getarccoords(&a);
-      circle(a.xstart,a.ystart,25);
-      setcolor(BLACK);
-      arc(getmaxx()/2,getmaxy()/2,angle,angle+2,150);
-      getarccoords(&a);
-      setcolor(GREEN);
-      circle(a.xstart,a.ystart,25);
-      angle = angle+5;
-      delay(50);
-   }
-   getch();
-   closegraph();
+	initwindow(300, 300);					// init window graphics
+	setbkcolor(1);							// set background
+   	cleardevice();
+	setcolor(14);							// set text color
+	outtextxy(50,100,"Graphics in Dev-C++");// print text in window graphics
+	while(!kbhit()) delay(1);				// pause screen	
 }
